65f1ce56e9a6969a2ff2defb3797c0d2352e9a8d
An Nguyen <truongan6@gmail.com>
2014-08-16 22:14:26 +0700

    updated some plugin for need

---
+++ app/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name generatorAngularApp
+ * @description
+ * # generatorAngularApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('generatorAngularApp', []);
